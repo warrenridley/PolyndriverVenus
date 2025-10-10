28bf6a2e42b3d3f648701d8757809c53d4d09cec
warrenridley <54641428+warrenridley@users.noreply.github.com>
2025-10-09 21:06:03 -0400

    Update dbus-sma.py

--- dbus-sma/dbus-sma.py
+++ dbus-sma/dbus-sma.py
@@ -233,7 +233,7 @@ class SmaDriver:
         }
     }
 
-    self._dbusmonitor = self._create_dbus_monitor(dbus_tree, valueChangedCallback=self._dbus_value_changed)
+    self._dbusmonitor = self._create_dbus_monitor(dbus_tree, valueChangedCallback=self._dbus_value_changed, acceptNoDeviceInstance=True)
 
     self._dbusservice = self._create_dbus_service()
 
